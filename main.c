961ded2983f3e6af903c5a1b3c7025ae5c420c58
Andrey Nechesov <alnechesov@edu.hse.ru>
2023-04-20 22:16:27 +0300

    Написал логику работы садовников

--- main.c
+++ main.c
@@ -2,22 +2,23 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <semaphore.h>
 
-int array[10][10];
-sem_t *semaphores[25];
-
 int main()
 {
     const int COLUMNS = 10;
     const int ROWS = 10;
+    const int BIG_COLUMNS = 5;
+    const int BIG_ROWS = 5;
     const int field_size = ROWS * COLUMNS;
     const int pID = 0;
     const char *shar_object = "posix-shar-object";
+    char *sempahore_template_name = "garden-semaphore-id-";
     int shmid;
     // Будем перемещаться как в шарпах: array[i, j] = array[i * COLUMNS + j]
     int *field;
@@ -50,13 +51,133 @@ int main()
         }
     }
 
-    for (int i = 0; i < ROWS; ++i)
+    for (int k = 0; k < COLUMNS * ROWS / 4; ++k)
     {
-        for (int j = 0; j < COLUMNS; ++j)
+        char sem_name[200];
+        sprintf(sem_name, "%s%d", sempahore_template_name, k);
+
+        sem_t *sem;
+        if ((sem = sem_open(sem_name, O_CREAT, 0666, 1)) == 0)
         {
-            printf("%d ", field[i * COLUMNS + j]);
-            fflush(stdout);
-            sleep(10);
+            perror("sem_open: Can not create admin semaphore");
+            exit(-1);
+        };
+
+        int val;
+        sem_getvalue(sem, &val);
+        printf("Val_%d: %d\n", k, val);
+    }
+
+    // first worker
+
+    pid_t chpid1 = fork();
+    if (chpid1 == 0)
+    {
+        sem_t *semaphores[25];
+        for (int k = 0; k < COLUMNS * ROWS / 4; ++k)
+        {
+            char sem_name[200];
+            sprintf(sem_name, "%s%d", sempahore_template_name, k);
+
+            if ((semaphores[k] = sem_open(sem_name, 0)) == 0)
+            {
+                perror("sem_open: Can not open semaphore");
+                exit(-1);
+            };
         }
+
+        int i = 0;
+        int j = 0;
+        while (i < ROWS)
+        {
+            while (j < COLUMNS)
+            {
+                sem_wait(semaphores[i / 2 * BIG_COLUMNS + j / 2]);
+                printf("First gardener takes %d plot\n", i * COLUMNS + j);
+                sem_post(semaphores[i / 2 * BIG_COLUMNS + j / 2]);
+                ++j;
+            }
+
+            ++i;
+            --j;
+
+            while (j >= 0)
+            {
+                sem_wait(semaphores[i / 2 * BIG_COLUMNS + j / 2]);
+                printf("First gardener takes %d plot\n", i * COLUMNS + j);
+                sem_post(semaphores[i / 2 * BIG_COLUMNS + j / 2]);
+                --j;
+            }
+
+            ++i;
+            ++j;
+        }
+
+        exit(0);
+    }
+    pid_t chpid2 = fork();
+
+    // second worker
+    if (chpid2 == 0)
+    {
+        sem_t *semaphores[25];
+        for (int k = 0; k < COLUMNS * ROWS / 4; ++k)
+        {
+            char sem_name[200];
+            sprintf(sem_name, "%s%d", sempahore_template_name, k);
+
+            if ((semaphores[k] = sem_open(sem_name, 0)) == SEM_FAILED)
+            {
+                perror("sem_open: Can not open semaphore");
+                exit(-1);
+            };
+        }
+
+        int i = ROWS - 1;
+        int j = COLUMNS - 1;
+        while (j >= 0)
+        {
+            while (i >= 0)
+            {
+                int val;
+                sem_wait(semaphores[i / 2 * BIG_COLUMNS + j / 2]);
+                printf("Second gardener takes %d plot\n", i * COLUMNS + j);
+                sem_post(semaphores[i / 2 * BIG_COLUMNS + j / 2]);
+                --i;
+            }
+
+            --j;
+            ++i;
+
+            while (i < ROWS)
+            {
+                sem_wait(semaphores[i / 2 * BIG_COLUMNS + j / 2]);
+                printf("Second gardener takes %d plot\n", i * COLUMNS + j);
+                sem_post(semaphores[i / 2 * BIG_COLUMNS + j / 2]);
+                ++i;
+            }
+
+            --i;
+            --j;
+        }
+
+        exit(0);
+    }
+
+    int status = 0;
+    waitpid(chpid1, &status, 0);
+    waitpid(chpid2, &status, 0);
+
+    for (int k = 0; k < COLUMNS * ROWS / 4; ++k)
+    {
+        char sem_name[200];
+        sprintf(sem_name, "%s%d", sempahore_template_name, k);
+
+        sem_close(sem_open(sem_name, 0));
+        if (sem_unlink(sem_name) < 0)
+        {
+            perror("Can not unlink semaphore");
+            exit(-1);
+        };
     }
 }
